483af219f95cc3c8728df90f2348c8acfab6f73d
iamshiwei <2587159769@qq.com>
2021-05-17 22:21:44 +0800

    添加全局拦截

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router/index";
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+Vue.use(VueAxios, axios);
 
 Vue.config.productionTip = false;
+
+axios.defaults.baseURL = "/api";
+axios.defaults.timeout = 8000;
+// 接口错误拦截
+axios.interceptors.response.use(function (response) {
+  let res = response.data;
+  if (res.status == 0) {
+    return res.data;
+  } else if (res.status == 10) {
+    window.location.href = "/#/login";
+  } else {
+    alert(res.msg);
+  }
+});
 
 new Vue({
   router,
